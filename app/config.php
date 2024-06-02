de437362419b67ada5d9a48e3582c73d7ca7b8d9
Madgamermlg <120388991+Madgamermlg@users.noreply.github.com>
2024-06-02 21:04:02 +0200

    Initial commit with files

---
+++ app/config.php
@@ -0,0 +1,10 @@
+<?php
+// Basic connection settings
+$databaseHost = 'db';
+$databaseUsername = 'DennisM';
+$databasePassword = 'Eternatus!';
+$databaseName = 'rolodex';
+
+// Connect to the database
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+?>
